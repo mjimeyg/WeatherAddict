71731da97db173123b4409c7c23b8efb69691e4e
mjimeyg <a14hg5@hotmail.co.uk>
2016-03-25 14:14:06 +0000

    Initial Commit

---
+++ app/src/main/java/mjimeyg/weatheraddict/Overview.java
@@ -0,0 +1,13 @@
+package mjimeyg.weatheraddict;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Overview extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
